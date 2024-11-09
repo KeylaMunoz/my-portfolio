8e01855ff924d55197c4156c2bed073e1e580c57
Keyla Muñoz <keyla.keyla.munoz@gmail.com>
2024-11-09 13:26:47 -0300

    actualizacion wsp

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,7 +12,7 @@ import { useState } from 'react';
                         <img src="https://img.icons8.com/color/512/linkedin.png" className="icon linkedin" />
                     </div>
                     <div className="icon-back">
-                        <a href="https://www.linkedin.com/in/keyla-abigail-mu%C3%B1oz-1b1755287/" target="_blank">
+                        <a href="https://www.linkedin.com/in/keyla-abigail-mu%C3%B1oz-1b1755287/  https://wa.me/5493811234567?text=Hola%2C%20estoy%20interesado%20en%20tus%20productos" target="_blank">
                             <p>Linkedin Keyla Muñoz</p>
                         </a>
                     </div>
@@ -43,7 +43,9 @@ import { useState } from 'react';
                         <img src="https://cdn-icons-png.freepik.com/256/3536/3536445.png?uid=R170152393&ga=GA1.1.233634873.1729727867&semt=ais_hybrid" alt="WhatsApp Icon" className="icon" />
                     </div>
                     <div className="icon-back">
-                        <p>+ 54 9 381 3644789</p>
+                        <a href=" https://wa.me/5493813644789?text=Hola%20Keyla%20Muñoz2C%20estoy%20interesado%20en%20tu%20perfil" target="_blank">
+                            <p>+54 9 3813644789</p>
+                        </a>
                     </div>
                 </div>
             </div>
